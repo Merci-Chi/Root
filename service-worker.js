f4be9bef91d732cc55d30039148a5173734629db
Merci-Chi <bdzzsx8vrq@privaterelay.appleid.com>
2025-12-22 13:33:09 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,10 @@
+const CACHE_NAME = "daily-notes-cache-v1";
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("pwa-cache").then(cache => cache.addAll([
-      "./",
-      "./index.html",
-      "./app.js",
-      "./manifest.json"
-    ]))
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll(["./", "./index.html", "./app.js", "./manifest.json"])
+    )
   );
 });
 
